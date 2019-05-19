cf9ab0d893fba59015929325d8e625580b8e1d56
Mark Hernandez <m.hern1415@gmail.com>
2019-05-19 21:37:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -17,11 +17,12 @@ class Owner
     @pets[:fishes] << Fish.new(name_of_fish)
   end
     
-  def buy_cat
+  def buy_cat(name_of_cat)
     @pets[:cats] << Cat.new(name_of_cat)
   end
   
-  def buy_dog
+
+  def buy_dog(name_of_dog)
     @pets[:dog] << Dog.new(name_of_dog)
   end 
   
